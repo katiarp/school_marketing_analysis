a0f0dab16eaf66b1e8dd577c0dd7fb13fc8257da
katiarp <katia.paredes84@gmail.com>
2024-01-14 21:41:44 -0800

    Updated README.md and visualizaion files

--- README.md
+++ README.md
@@ -1,30 +1,32 @@
 # Project Description
 
 
-The goal of this project is to show clusters of geographic areas that meet specific socioeconomic and demographic conditions. The Census data API was used to get the data of interest at the block group and block geographies as defined by the Census bureau.
+The goal of this project is to maximize marketing and outreach resource allocation by showing clusters of geographic areas that meet specific socioeconomic and demographic conditions. By using this data analysis, any decision maker can visualize where their efforts could have a higher return of investment.
+
+This project was based on data obtained from the Census data API at the Tract level. The dataset used was the American Community Survey: 5-Year Estimates: Detailed Tables 5-Year 2022, which at the time of this writing was the most recent (for more information go to the datasets section.) 
 
 ## Understanding Geographies
 
-All datasets available through the Census data api can be queried at different geographies levels. The Census provide these geographies:      
- 
+All datasets available through the Census data API can be queried at different geographies levels (for more information go to the  [Understanding Geographies ](https://www.census.gov/newsroom/blogs/random-samplings/2014/07/understanding-geographic-relationships-counties-places-tracts-and-more.html)section). Another useful visualization of geographic boundaries can be found [here](https://www.caliper.com/maptitude/census2000data/summarylevels.htm). 
+
+The United States Census Bureau provide these geographies:      
 
 ![](https://www.census.gov/content/dam/Census/newsroom/blogs/2014/07/understanding-geographic-relationships-counties-places-tracts-and-more/geo1.jpg)
 
-https://www.census.gov/newsroom/blogs/random-samplings/2014/07/understanding-geographic-relationships-counties-places-tracts-and-more.html
 
-https://www.caliper.com/maptitude/census2000data/summarylevels.htm
+
 
 
 ## Datasets
 
+The geography of interest will determine the dataset to be used because not all datasets contain data at all geographies.
 
-The American Community Survey: 5-Year Estimates: Detailed Tables 5-Year (acs/acs5) was used to get income and demographic data at the block group level and the block level.
+The American Community Survey: 5-Year Estimates: Detailed Tables 5-Year (acs/acs5) was used to get income and demographic data at the tract level.
+
 
 ## Variables
 
-The Census data API allows to specify the variables (i.e. columns) of interest to be in the response. 
+The variable groups used for this analysis were the following:
 
-The following variables were chosen from the acs/acs5 data set:
-
-* Variable 1: description of this variable
-* Variable 2: description of this variable
+* B19001: Household Income in the Past 12 Months (in 2022 Inflation-Adjusted Dollars)
+* B09002: Own Children Under 18 Years by Family Type and Age
--- static/js/demographics_map.js
+++ static/js/demographics_map.js
@@ -1,35 +1,115 @@
 
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v12/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+
+
+// Create a base layer that holds maps
+let baseMaps = {
+  "Day": day,
+  "Satellite": satellite
+};
+// Create layers
+let percent = new L.LayerGroup();
+let children = new L.LayerGroup();
+
+let overlays = {
+  "Income greater than $200,000": percent,
+  "Income $150,000 to $199,999": percent,
+  "Income $125,000 to $149,999": percent,
+  "Income $100,000 to $124,999": percent,
+  "Children Ages": children,
+};
 
 // Creating a map object
 var myMap = L.map("map", {
   center: [32.7157, -117.1611],
-  zoom: 11
+  zoom: 12,
+  layers: [day]
 });
 
-
-
-// Adding a tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
+// Control map that allows to change which layers are visible.
+L.control.layers(baseMaps, overlays, {
+  collapsed: true
 }).addTo(myMap);
 
-// If data.beta.nyc is down comment out this link
-var link = "static/data/demographics_geo_json.geojson";
+
+// Data
+var link = "static/data/asc5_demographics_tract.geojson";
 
 
-// Use this link to get the geojson data.
-// var link = "static/data/nyc.geojson";
+// purple orange green
+function chooseColor(percent) {
+  console.log('percent: ' + percent)
+  if (percent >= 0 && percent < 30){
+    return "#4ef1ff";
+  }
+  else if (percent >= 30 && percent < 40){
+    return "#01ebff";
+  }
+  else if (percent >= 40 && percent < 50){
+    return "#00d5e7";
+  }
+  else if (percent >= 50 && percent < 60){
+    return "#00bece";
+  }
+  else if (percent >= 60 && percent < 70){
+    return "#00a6b4";
+  }
+  else if (percent >= 70 && percent < 80){
+    return "#008e9b";
+  }
+  else if (percent >= 80 && percent < 90){
+    return "#007781";
+  }
+  else if (percent >90){
+    return "#005f68";
+  }
+    else{
+      return "#dad4c7"}
+
+
+  // // switch (income) {
+  // case income > 50:
+  //   return "#800076";
+  //   default:"#00cd10"
+  // }
+}
+
 
 // Get our GeoJSON data using d3.json
-d3.json(link).then(function(data) {
+d3.json(link).then(function(data){
   // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(myMap);
+  L.geoJson(data,{
+    onEachFeature: function(feature, layer){
+      layer.bindPopup("<h3>Income greater than $200k: " + feature.properties.percentage_income_greater_than_200000 + "%</h3> <hr> <h4>Tract: " + feature.properties.name + "</h4>")
+
+    },
+   style: function(feature){
+    return{
+      color: "#000000",
+      fillColor:chooseColor(feature.properties.percentage_income_greater_than_200000),
+      fillOpacity: 0.75,
+      weight: 1
+    }
+  }
+}
+    ).addTo(percent);
+    percent.addTo(myMap);
 });
+
+
+
+
 
 
 
